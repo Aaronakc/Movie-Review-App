71f835376b21e4c6acfb6353df0399da22e6a490
Aarona KC <aaronakc99@gmail.com>
2025-08-05 01:15:48 +0545

    updated login screen

--- src/Components/InputElement.tsx
+++ src/Components/InputElement.tsx
@@ -5,14 +5,15 @@ import React from 'react'
 interface InputElementProps {
   placeholder: string;
   icon: any;
+  backgroundColor:string;
 }
 
-const InputElement = ({ placeholder, icon }: InputElementProps) => {
+const InputElement = ({ placeholder, icon,backgroundColor }: InputElementProps) => {
   return (
     <View style={styles.container}>
-      <View style={styles.inputContainer}>
+      <View style={[styles.inputContainer,{backgroundColor:backgroundColor}]}>
         <Image source={icon} style={styles.icon} />
-        <TextInput style={styles.input} placeholder={placeholder} placeholderTextColor='white' />
+        <TextInput style={styles.input} placeholder={placeholder} placeholderTextColor='#d3d1d1ff' />
       </View>
 
     </View>
@@ -22,15 +23,15 @@ const InputElement = ({ placeholder, icon }: InputElementProps) => {
 const styles = StyleSheet.create({
   container: {
     width: 300,
-    marginBottom: 7,
-    marginTop: 15,
+    marginBottom: 15,
+    // marginTop: 15,
     marginLeft: 17,
     marginRight: 17,
 
   },
   inputContainer: {
     flexDirection: "row",
-    backgroundColor: "gray",
+    // backgroundColor: "gray",
     borderRadius: 14,
     borderWidth: 0.2,
     borderColor: "white",
