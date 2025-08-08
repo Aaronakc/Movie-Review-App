e79e116fa797227089dab96915b9bfb910ce6f01
Aarona KC <aaronakc99@gmail.com>
2025-08-08 13:58:20 +0545

    completed the search page ui

--- src/Screens/SearchScreen.tsx
+++ src/Screens/SearchScreen.tsx
@@ -1,9 +1,11 @@
-import { View, Text, TextInput, StyleSheet, ScrollView } from 'react-native'
+import { View, Text, TextInput, StyleSheet, ScrollView, Image, TouchableOpacity } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import MovieCarousel from '../Components/MovieCarousel'
 import { searchMovie } from '../utils/FilterMovie'
+import IconTextRow from '../Components/IconTextRow'
+import { HomeTabScreenProps } from '../types/NavigationTypes'
 
-const SearchScreen = () => {
+const SearchScreen = ({ navigation }: HomeTabScreenProps<'Search'>) => {
   const [search, setSearch] = useState('')
   const [results, setResults] = useState([])
 
@@ -38,13 +40,38 @@ const SearchScreen = () => {
 
   return (
     <ScrollView style={styles.container}>
-      <TextInput style={styles.input} placeholder="Search" placeholderTextColor={"white"} onChangeText={(text) => setSearch(text)} />
-      {
-        results.length > 0
-        &&
-        <MovieCarousel topic="Search" searchedMovies={results} />
-      }
-      {/* <MovieCarousel topic="Trending" endpoint='trending' /> */}
+      <View>
+        <TextInput style={styles.input} placeholder="Search.." placeholderTextColor={"white"} onChangeText={(text) => setSearch(text)} />
+        {
+          results.length > 0
+          &&
+          <MovieCarousel topic="Search" searchedMovies={results} />
+        }
+
+      </View>
+      <Image source={require('../../assets/searchIcon.png')} style={styles.searchIcon} />
+
+
+      <View style={{ marginTop: 40, marginLeft: 3 }}>
+        <MovieCarousel topic="Trending this month" endpoint='trending' />
+      </View>
+
+      <View style={styles.backgroundWrapper}>
+        <IconTextRow icon={require('../../assets/movieIcon.png')} text='BROWSE ALL MOVIES' width={61} height={61} fontSize={16} color='white' />
+      </View>
+
+      <TouchableOpacity onPress={() => {
+        navigation.navigate('BottomHome')
+        console.log('reached home screen')
+      }} style={styles.touchableContainer} >
+        <Image source={require('../../assets/forwardArrow.png')} style={styles.arrow} />
+      </TouchableOpacity>
+
+
+      <View style={{ marginTop: 60, marginLeft: 3 }}>
+        <MovieCarousel topic="Upcoming movies" endpoint='upcoming' />
+      </View>
+
     </ScrollView>
   )
 }
@@ -55,16 +82,50 @@ const styles = StyleSheet.create({
     backgroundColor: "#002335",
 
   },
+  searchIcon: {
+    width: 24,
+    height: 24,
+    marginTop: -35,
+    marginLeft: 290,
+  },
   input: {
-    borderWidth: 0.8,
+    width:300,
+    height:40,
+    borderWidth: 0.3,
     borderColor: "white",
     backgroundColor: "gray",
+    opacity:0.8,
     marginHorizontal: 30,
-    marginTop: 10,
+    marginTop: 35,
     borderRadius: 10,
     paddingLeft: 10,
+    paddingRight: 50,
 
+  },
+  backgroundWrapper: {
+    backgroundColor: "gray",
+    opacity:0.8,
+    width: 334,
+    height: 83,
+    borderRadius: 14,
+    marginLeft: 13,
+    paddingLeft: 23,
+    paddingTop: 11,
+    paddingBottom: 11,
+
+  },
+  touchableContainer: {
+    width: 22,
+    height: 22,
+    marginTop: -55,
+    marginLeft: 300,
+  },
+  arrow: {
+    width: "100%",
+    height: "100%",
+    objectFit: "contain",
   }
+
 
 })
 
